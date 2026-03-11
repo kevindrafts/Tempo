50c30eadd14df3e2ecc2dba3843d6f06d37d8f57
kevindrafts <124643321+kevindrafts@users.noreply.github.com>
2026-03-11 12:18:09 -0400

    Add Sparkle auto-updates and CI release pipeline

--- Tempo/TempoApp.swift
+++ Tempo/TempoApp.swift
@@ -1,10 +1,21 @@
 import SwiftUI
 import EventKit
+import Sparkle
 
 @main
 struct TempoApp: App {
     @NSApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
     @StateObject private var eventKitManager = EventKitManager.shared
+
+    private let updaterController: SPUStandardUpdaterController
+
+    init() {
+        updaterController = SPUStandardUpdaterController(
+            startingUpdater: true,
+            updaterDelegate: nil,
+            userDriverDelegate: nil
+        )
+    }
 
     var body: some Scene {
         WindowGroup {
@@ -15,13 +26,44 @@ struct TempoApp: App {
         .windowStyle(.titleBar)
         .commands {
             TempoCommands()
+
+            CommandGroup(after: .appInfo) {
+                CheckForUpdatesView(updater: updaterController.updater)
+            }
         }
     }
 }
 
+// MARK: - Check for Updates (Sparkle)
+
+struct CheckForUpdatesView: View {
+    @ObservedObject private var checkForUpdatesViewModel: CheckForUpdatesViewModel
+    private let updater: SPUUpdater
+
+    init(updater: SPUUpdater) {
+        self.updater = updater
+        self.checkForUpdatesViewModel = CheckForUpdatesViewModel(updater: updater)
+    }
+
+    var body: some View {
+        Button("Check for Updates…", action: updater.checkForUpdates)
+            .disabled(!checkForUpdatesViewModel.canCheckForUpdates)
+    }
+}
+
+final class CheckForUpdatesViewModel: ObservableObject {
+    @Published var canCheckForUpdates = false
+
+    init(updater: SPUUpdater) {
+        updater.publisher(for: \.canCheckForUpdates)
+            .assign(to: &$canCheckForUpdates)
+    }
+}
+
+// MARK: - App Delegate
+
 class AppDelegate: NSObject, NSApplicationDelegate {
     func applicationDidFinishLaunching(_ notification: Notification) {
-        // Configure window appearance
         if let window = NSApplication.shared.windows.first {
             window.setContentSize(NSSize(width: 1100, height: 700))
         }
